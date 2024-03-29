4cc331691d1927881de1da531f5df9476bb9a4bc
shantanuparabumd <sparab@umd.edu>
2024-03-28 20:47:00 -0400

    Initilatize

---
+++ main.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set up the figure and axis
+fig, ax = plt.subplots()
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+
+# Create the ball
+ball = plt.Circle((-5, -5), 0.5, color='r')
+
+# Add the ball to the plot
+ax.add_patch(ball)
+
+
+def move(theta, distance):
+    x = distance * np.cos(theta)
+    y = distance * np.sin(theta)
+    return x, y
+
+def check_boundary(x, y):
+    if x+0.5 > 10 or x-0.5 < -10:
+        return True
+    if y+0.5 > 10 or y-0.5 < -10:
+        return True
+    
+    return False
+
+theta = np.pi/12
+speed = 0.5
+# Update the ball position at each iteration
+try:
+    while True:
+        # Generate random displacements for x and y coordinates
+        dx, dy = move(theta, speed)
+        
+        # Check if the ball hits the boundary
+        if check_boundary(ball.center[0] + dx, ball.center[1] + dy):
+            # Reverse the direction of theta to make the ball rebound
+            # theta = np.pi/2 + theta
+            theta = np.random.uniform(0, 2*np.pi)
+            dx, dy = move(theta, speed)
+        else:
+            # Update the ball position
+            ball.center = (ball.center[0] + dx, ball.center[1] + dy)
+        
+        # Redraw the plot
+        plt.pause(0.1)
+        plt.draw()
+
+except KeyboardInterrupt:
+    # Handle keyboard interrupt
+    print("Keyboard interrupt detected. Exiting...")
+    
+    # Update the ball position
+    ball.center = (ball.center[0], ball.center[1])
+    
+    # Redraw the plot
+    plt.pause(0.1)
+    plt.draw()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# To-Do List Using React JS, Redux and Webpack
+
+## Author
+
+|Name|ID|Email|
+|:---:|:---:|:---:|
+|Shantanu Parab|119208625|sparab@umd.edu|
+
+
+## Overview
+Brownian Motion is the random movement of particles in a fluid, resulting from their collision with other atoms or molecules. In certain robot applications such as exploration, coverage, and swarm dynamics, simulating Brownian Motion behavior can be beneficial. This challenge involves implementing and simulating Brownian Motion behavior on a robot.
+
+
+## Usage
+
+1. Run the program
+```ssh
+python3 main.py
+```
+
+## Demonstration
